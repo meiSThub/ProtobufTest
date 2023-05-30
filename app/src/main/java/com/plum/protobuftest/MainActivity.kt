fb0b62689839697d3a1e67b9bbacb93d7c3edf34
meixianbing <xianbing.mei@megatronix.co>
2023-05-30 21:31:44 +0800

    android 模块依赖 使用了protobuf的 android module 和 纯java module

--- app/src/main/java/com/plum/protobuftest/MainActivity.kt
+++ app/src/main/java/com/plum/protobuftest/MainActivity.kt
@@ -2,10 +2,31 @@ package com.plum.protobuftest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.plum.proto.data.StudentOuterClass
+import com.plum.proto.java.lib.WeatherOuterClass
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // android protobuf library module
+        val student = StudentOuterClass.Student.newBuilder()
+            .setName("张三")
+            .setAge(18)
+            .build()
+        Log.i(TAG, "onCreate: student=$student")
+
+        // java protobuf library module
+        val weather = WeatherOuterClass.Weather.newBuilder()
+            .setQuery(2)
+            .setSeasonValue(30)
+            .build()
+        Log.i(TAG, "onCreate: student=$weather")
     }
 }
